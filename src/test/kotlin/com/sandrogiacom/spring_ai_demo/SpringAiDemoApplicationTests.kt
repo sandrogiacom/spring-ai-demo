f4a80eab2be8c69b6595a5502bf2cbff02268b47
sandro <sandrogiacom@gmail.com>
2026-06-07 07:33:01 -0300

    chore: Add ActiveProfiles for testing and update dependencies for OpenAPI and H2 support

--- src/test/kotlin/com/sandrogiacom/spring_ai_demo/SpringAiDemoApplicationTests.kt
+++ src/test/kotlin/com/sandrogiacom/spring_ai_demo/SpringAiDemoApplicationTests.kt
@@ -2,8 +2,10 @@ package com.sandrogiacom.spring_ai_demo
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringAiDemoApplicationTests {
 
 	@Test
